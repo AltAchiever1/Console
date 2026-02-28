4e405e483aafa543ea8eecff14a56ada4da4fa1f
AltAchiever1 <tylerbriggsbest@gmail.com>
2026-02-28 18:59:41 +1100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+
+namespace Console
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start() => Console.LoadConsole();
+    }
+}
